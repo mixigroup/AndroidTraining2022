4acb48e4da6584b2541bc011cbebda9623e26107
Atsuto Yamada <atsuto.yamada@mixi.co.jp>
2022-04-23 14:22:19 +0900

    initial commit

---
+++ app/src/main/java/jp/co/mixi/androidtraining2022/LapTime.kt
@@ -0,0 +1,6 @@
+package jp.co.mixi.androidtraining2022
+
+data class LapTime(
+    val number: Int,
+    val time: Long
+)
---
+++ app/src/main/java/jp/co/mixi/androidtraining2022/TransitionType.kt
@@ -0,0 +1,5 @@
+package jp.co.mixi.androidtraining2022
+
+enum class TransitionType {
+    START, END
+}
