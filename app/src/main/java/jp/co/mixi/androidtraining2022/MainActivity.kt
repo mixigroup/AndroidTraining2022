2113ab472e88fb0c6ae23fc393e25ff9ef28532e
yamagai <kohei.yamamoto919@gmail.com>
2022-04-26 16:35:36 +0900

    lesson7 実装

--- app/src/main/java/jp/co/mixi/androidtraining2022/MainActivity.kt
+++ app/src/main/java/jp/co/mixi/androidtraining2022/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import jp.co.mixi.androidtraining2022.databinding.ActivityMainBinding
+import java.sql.Time
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val viewModel by viewModels<MainViewModel>()
+    private val lapTimeList = mutableListOf<LapTime>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,13 +21,6 @@ class MainActivity : AppCompatActivity() {
         val adapter = LapTimeAdapter()
         binding.recyclerView.adapter = adapter
 
-        // FIXME 一時的にデータを入れておく
-        adapter.submitList(
-            List(10) {
-                LapTime(it + 1, 0)
-            }
-        )
-
         // 経過時間を変更
         viewModel.currentTimeText.observe(this) {
             binding.timeText.text = it
@@ -35,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         viewModel.primaryButtonType.observe(this) { type ->
             when (type) {
                 PrimaryButtonType.TIMER_START -> {
+                    binding.secondaryButton.isEnabled = false
                     binding.primaryButton.setText(R.string.start)
                     binding.primaryButton.backgroundTintList = getColorStateList(R.color.primary)
                     binding.primaryButton.setOnClickListener {
@@ -42,6 +38,7 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
                 PrimaryButtonType.TIMER_STOP -> {
+                    binding.secondaryButton.isEnabled = true
                     binding.primaryButton.setText(R.string.stop)
                     binding.primaryButton.backgroundTintList = getColorStateList(R.color.accent)
                     binding.primaryButton.setOnClickListener {
@@ -49,20 +46,27 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
                 PrimaryButtonType.TIMER_CLEAR -> {
+                    binding.secondaryButton.isEnabled = false
                     binding.primaryButton.setText(R.string.clear)
                     binding.primaryButton.backgroundTintList = getColorStateList(R.color.primary_variant)
                     binding.primaryButton.setOnClickListener {
                         viewModel.state.value = State.CLEAR
+                        lapTimeList.clear()
                     }
                 }
             }
         }
 
         binding.secondaryButton.setOnClickListener {
-            if (binding.root.currentState == R.id.start) {
-                binding.root.transitionToEnd()
-            } else {
+            val time = viewModel.currentTime.value ?: 0L
+            val lap = LapTime(lapTimeList.size + 1, time)
+            lapTimeList.add(lap)
+
+            if (lapTimeList.isEmpty()) {
                 binding.root.transitionToStart()
+            } else {
+                adapter.submitList(lapTimeList)
+                binding.root.transitionToEnd()
             }
         }
     }
